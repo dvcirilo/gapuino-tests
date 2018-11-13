75f363e1d6ff677380dd2cfa2479b072ed94f56b
Diego Cirilo <dvcirilo@gmail.com>
2018-11-13 16:56:23 +0000

    adding vagrant

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.provider "virtualbox" do |vb|
+    # Passing the USB port of Gapuino to VM 
+    # Check on host with "VBoxManage list usbhost"
+    # VendorId: 0x0403 and ProductId: 0x6010
+    vb.customize ["modifyvm", :id, "--usb", "on"]
+    vb.customize ['usbfilter', 'add', '0', '--target', :id, '--name', 'ESP',
+                  '--vendorid', '0x0403', '--productid', '0x6010']
+  end
+  # Script that installs the software needed
+  config.vm.provision :shell, path: "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+apt update
+apt dist-upgrade -y
+apt install -y build-essential git libftdi-dev libftdi1 doxygen python3-pip libsdl2-dev curl cmake libusb-1.0-0-dev
+ln -s /usr/bin/libftdi-config /usr/bin/libftdi1-config
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt install git-lfs
+git lfs install
+pip3 install pyelftools
+usermod -a -G dialout vagrant
+
+touch 90-ftdi_gapuino.rules
+echo 'ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE="0666", GROUP="dialout"'> 90-ftdi_gapuino.rules
+mv 90-ftdi_gapuino.rules /etc/udev/rules.d/
+udevadm control --reload-rules && udevadm trigger
+git clone https://github.com/GreenWaves-Technologies/gap_sdk.git /opt/gap_sdk
+git lfs clone https://github.com/GreenWaves-Technologies/gap_riscv_toolchain.git /usr/lib/gap_riscv_toolchain
+
+cd /usr/lib/gap_riscv_toolchain/bin
+for eachFile in *
+do
+    ln -fs $PWD/$eachFile /usr/bin/$eachFile
+done
+
+cd /opt/gap_sdk
+git submodule update --init --recursive
+
+source sourceme.sh
+make all
+
+echo "source /opt/gap_sdk/sourceme.sh" >> /home/vagrant/.bashrc
+
+echo "Done! :)"
