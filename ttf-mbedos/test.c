9285b2f7b94ee195b11f87cb79d076c590e418cb
Diego Cirilo <dvcirilo@gmail.com>
2018-12-06 13:54:34 +0000

    [WIP] getting timer to work

--- ttf-mbedos/test.c
+++ ttf-mbedos/test.c
@@ -53,14 +53,17 @@ void test_rand(int *L1_mem, int frequency, int voltage)
 
     for(int j=0;j<NUM_TESTS;j++) {
         __disable_irq();
+        Timer_Initialize(TIMER1_CLUSTER, 0);
+        Timer_Enable(TIMER1_CLUSTER);
         begin = clock();
         printf("run %d ", j);
         for(int i=0;i<RUNS;i++) {
             CLUSTER_SendTask(0, Master_Entry, (void *) L1_mem, 0);
             CLUSTER_Wait(0);
             if (L1_mem[0]^L1_mem[1]^L1_mem[2]^L1_mem[3]^L1_mem[4]^L1_mem[5]^L1_mem[6]^L1_mem[7]){
-                printf("diff after: %d clocks - %d calls\n", (clock()-begin), i);
+                printf("diff after: %d clocks - %d calls\n", (int)Timer_ReadCycle(TIMER1_CLUSTER)/Timer_GetTicksUS(TIMER1_CLUSTER), i);
                 error = 1;
+                printf("ticks: %d\n", Timer_GetTicksUS(TIMER1_CLUSTER));
                 break;
             } else {
                 error = 0; 
@@ -68,9 +71,10 @@ void test_rand(int *L1_mem, int frequency, int voltage)
         }
         __enable_irq();
         if(!error){
-            printf("Run %dk times without diff\n", RUNS/1000);
+            printf("Run %dk times without diff - time:%d \n", RUNS/1000, (int)Timer_ReadCycle(TIMER1_CLUSTER)/Timer_GetTicksUS(TIMER1_CLUSTER));
             error = 0;
         }
+        Timer_Disable(TIMER1_CLUSTER);
     }
 }
 
@@ -88,7 +92,7 @@ int main()
 
     int *L1_mem = L1_Malloc(8);
 
-    test_rand(L1_mem, 217000000, 1000);
+    test_rand(L1_mem, 218000000, 1000);
 
     /* Cluster Stop - Power down */
     CLUSTER_Stop(0);
